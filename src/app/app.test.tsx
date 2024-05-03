d5248d3847efe46b9d940cbd05b862394d38cc01
Morgan Worrell <hello@morganism.codes>
2024-05-02 18:32:22 -0600

    adds initial test

---
+++ src/app/app.test.tsx
@@ -0,0 +1,9 @@
+import { expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import HomePage from './page';
+
+test('HomePage', () => {
+  // render(<HomePage />);
+  // expect(screen.getByRole('heading', { level: 1, name: 'Home' })).toBeDefined();
+  expect(true);
+});
