31a7dcdb008741fc0a34a6db49bab34b1e273934
Morgan Worrell <hello@morganism.codes>
2024-05-02 18:25:53 -0600

    adds appwrite node client

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,12 +1,12 @@
 import getEnv from '@/utils/get-env';
 import { cookies } from 'next/headers';
-import { Account, Client } from 'node-appwrite';
+import { Account, Client, Databases } from 'node-appwrite';
 
 export const API_ENDPOINT = getEnv('NEXT_PUBLIC_APPWRITE_ENDPOINT');
 export const PROJECT_ID = getEnv('NEXT_PUBLIC_APPWRITE_PROJECT_ID');
 export const API_KEY = getEnv('NEXT_APPWRITE_KEY');
 
-export const SESSION_KEY = 'my-custom-session';
+export const SESSION_KEY = 'my-silent-shark-session';
 
 export async function createSessionClient() {
   const client = new Client().setEndpoint(API_ENDPOINT).setProject(PROJECT_ID);
@@ -38,3 +38,30 @@ export async function createAdminClient() {
     },
   };
 }
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    return await account.get();
+  } catch (error) {
+    return null;
+  }
+}
+
+export function getDatabasesClient(): Databases {
+  const session = cookies().get(SESSION_KEY);
+
+  if (!session || !session.value) {
+    throw new Error('No session');
+  }
+
+  const client = new Client()
+    .setEndpoint(API_ENDPOINT)
+    .setProject(PROJECT_ID)
+    .setSession(session.value);
+
+  return new Databases(client);
+}
+
+export { ID } from 'appwrite';
+export type { Models } from 'appwrite';
